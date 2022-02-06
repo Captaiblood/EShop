1144b73f73cea2720db991c45fbb47a474e705bf
Manik <Captaiblood@users.noreply.github.com>
2022-02-06 15:19:16 +0000

    Add Entities and Redis

---
+++ src/Services/Basket/Basket.API/Entities/ShopingCart.cs
@@ -0,0 +1,36 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShopingCart
+    {
+        public string? UserName { get; set; } 
+
+        public List<ShopingCartItem> shopingCartItems { get; set; } = new List<ShopingCartItem>();
+        public ShopingCart() { }       
+
+        
+        public ShopingCart(string username)
+        {
+            this.UserName = username;
+        }
+
+
+
+        public decimal TotalPrice
+        {
+            get
+            {
+
+                decimal totalPrice = 0;
+                foreach (var item in shopingCartItems)
+                {
+                    totalPrice = totalPrice * item.Price;
+                }
+                return totalPrice;
+
+            }
+
+        }
+
+
+    }
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShopingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShopingCartItem
+    {
+        public int Quantity { get; set; }
+        public int Price { get; set; } = 0;
+        public string? Colour { get; set; }
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+
+    }
+}
