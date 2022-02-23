184141c78ead3f1dd3e1c94478d33f99b2cda8a1
Manik <Captaiblood@users.noreply.github.com>
2022-02-23 09:16:51 +0000

    Setup Discount Service

---
+++ src/Services/Discount/Discount.API/Data/ApplicationDBContect.cs
@@ -0,0 +1,17 @@
+﻿using Discount.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.API.Data
+{
+    public class ApplicationDBContect:DbContext
+    {
+        public ApplicationDBContect(DbContextOptions<ApplicationDBContect> options):base(options)
+        {
+
+        }
+
+        //it is possible to simply initialize the property to null with the help of the null-forgiving operator (!):
+        //public DbSet<Coupon>  Coupons { get; set; } = null!;
+        public DbSet<Coupon> Coupons => Set<Coupon>();
+    }
+}
---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDiscription { get; set; } = string.Empty;
+        public int Amount { get; set; }
+
+       
+        
+    }
+}
